981eacb01471328ba4f37f8bfe3e6f835a0ed455
TheMaor2804 <mhgameing@Gmail.com>
2024-09-16 17:14:18 +0300

    Refactor Logged component to display user type in tooltip

--- src/layout/header/right-navigation/Logged.jsx
+++ src/layout/header/right-navigation/Logged.jsx
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import IconButton from "@mui/material/IconButton";
 import Avatar from "@mui/material/Avatar";
+import { Tooltip } from "@mui/material";
+import { useCurrentUser } from "../../../users/providers/UserProvider";
 
 
 export default function Logged() {
 
+  const { user } = useCurrentUser();
+
+  const [userType, setUserType] = useState(null);
+
+  useEffect(() => {
+    if (user.isBusiness) setUserType("Business");
+    else if (user.isAdmin) setUserType("Admin");
+    else setUserType("Guest");
+  }, [user]);
+
   return (
-    <IconButton
-      sx={{ p: 0, display: "inline-flex", marginLeft: 2 }}>
-      <Avatar alt="avatar" src="/images/avatar.png" />
-    </IconButton>
+    <Tooltip title={"Logged in as " + userType} >
+      <IconButton
+        sx={{ p: 0, display: "inline-flex", marginLeft: 2 }}>
+        <Avatar alt="avatar" src="/images/avatar.png" />
+      </IconButton>
+    </Tooltip>
   );
 }
