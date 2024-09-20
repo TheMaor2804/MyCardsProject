03bce003481babb9ffa938f76ec5afd590dda37f
TheMaor2804 <mhgameing@Gmail.com>
2024-09-20 11:49:04 +0300

    Refactor RightNavbar to add tooltip for dark mode button

--- src/layout/header/right-navigation/RightNavbar.jsx
+++ src/layout/header/right-navigation/RightNavbar.jsx
@@ -1,5 +1,5 @@
 import { useTheme } from "../../../providers/CustomThemeProvider";
-import { Box, IconButton } from "@mui/material";
+import { Box, IconButton, Tooltip } from "@mui/material";
 import React from "react";
 import DarkModeIcon from "@mui/icons-material/DarkMode";
 import LightModeIcon from "@mui/icons-material/LightMode";
@@ -28,9 +28,11 @@ export default function RightNavbar() {
       >
 
         <SearchBar />
-        <IconButton sx={{ ml: 1 }} onClick={toggleDarkMode}>
-          {isDark ? <LightModeIcon /> : <DarkModeIcon />}
-        </IconButton>
+        <Tooltip title={isDark ? "Light Mode" : "Dark Mode"}>
+          <IconButton sx={{ ml: 1 }} onClick={toggleDarkMode}>
+            {isDark ? <LightModeIcon /> : <DarkModeIcon />}
+          </IconButton>
+        </Tooltip>
         <Box sx={
           {
             display: { xs: "none", md: "inline-flex" },
