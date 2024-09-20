cb6e9b083dc2e866a2c7de95b3476978ec1df1a5
TheMaor2804 <mhgameing@Gmail.com>
2024-09-20 11:46:33 +0300

    Refactor AboutPage to add navigation links and improve styling

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,11 +1,25 @@
-import { Container, Typography } from "@mui/material";
+import { Container, Divider, Typography } from "@mui/material";
 import React from "react";
 import PageHeader from "../components/PageHeader";
 import { useTheme } from "../providers/CustomThemeProvider";
+import HomeIcon from '@mui/icons-material/Home';
+import AppRegistrationIcon from '@mui/icons-material/AppRegistration';
+import AddCardIcon from '@mui/icons-material/AddCard';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import LoginIcon from '@mui/icons-material/Login';
+import LogoutIcon from '@mui/icons-material/Logout';
+import AddIcon from "@mui/icons-material/Add";
+import StyleIcon from "@mui/icons-material/Style";
+import PortraitIcon from '@mui/icons-material/Portrait';
+import InfoIcon from "@mui/icons-material/Info";
+import SearchIcon from "@mui/icons-material/Search";
 
 export default function AboutPage() {
 
   const { isDark } = useTheme();
+
 
   return (
     <Container sx={{ color: isDark ? "#fff" : "#000" }}>
@@ -17,30 +31,131 @@ export default function AboutPage() {
         sx={{
           mt: 2,
           display: "flex",
-          alignItems: "center",
+          alignItems: "flex-start",
           justifyContent: "space-between",
           flexDirection: { xs: "column", md: "row" },
         }}
       >
         <Container sx={{ flex: 1, mr: 2 }}>
-          <Typography variant="body1" paragraph>
-            Welcome to our Business Cards Application! This platform allows users to create, view, and manage business cards with ease. Whether you are a regular user or a business user, our application offers a range of features to enhance your experience.
+          <Typography variant="h5" gutterBottom>
+            About
           </Typography>
           <Typography variant="body1" paragraph>
-            As a regular user, you can browse through a variety of business cards, like your favorite ones, and view them in your favorites section. Our search functionality makes it easy to find specific cards based on your interests.
+            This is a simple application that allows you to create, edit, delete, and view cards. You can also add cards to your favorites and view them later. You can also register and log in to the application to save your cards and view them later. The application also allows you to switch between light and dark themes. The application is built using React, Material-UI, and React Router. The application is responsive and works on mobile devices as well as desktops.
+          </Typography>
+          <Typography variant="h5" gutterBottom>
+            Navigation
           </Typography>
           <Typography variant="body1" paragraph>
-            Business users have additional privileges. You can create your own business cards, manage them, and showcase your business information to other users. This is a great way to promote your business and connect with potential clients.
+            You can navigate to different pages using the navigation bar at the
+            top or bottom of the page. The navigation bar contains the following links:
+          </Typography>
+          <Typography variant="h6" gutterBottom>
+            <StyleIcon /> Cards
           </Typography>
           <Typography variant="body1" paragraph>
-            Our application supports both dark mode and light mode, allowing you to choose the theme that best suits your preference. The user interface is designed to be intuitive and user-friendly, ensuring a seamless experience.
+            Clicking this link or icon will take you to the home page. <br />
+            Here, you can find various business cards.
+          </Typography>
+          <Typography variant="h6" gutterBottom>
+            <InfoIcon /> About
           </Typography>
           <Typography variant="body1" paragraph>
-            We use a robust API to handle all requests to our server, ensuring that your data is securely managed. Whether you are creating a new card, registering as a user, or retrieving data, our API ensures fast and reliable performance.
+            Clicking this link or icon will take you to the about page.
+          </Typography>
+          <Typography variant="h6" gutterBottom>
+            <FavoriteIcon /> My Favorites
           </Typography>
           <Typography variant="body1" paragraph>
-            Thank you for choosing our Business Cards Application. We are committed to providing you with the best experience possible. If you have any questions or feedback, please do not hesitate to contact us.
+            Clicking this link or icon will take you to the favorite cards page. <br />
+            You must be logged in to access this feature.
+            You can mark cards as favorites for quick access. To do this,
+            click the <FavoriteIcon /> on the card.
           </Typography>
+          <Typography variant="h6" gutterBottom>
+            <PortraitIcon /> My Cards
+          </Typography>
+          <Typography variant="body1" paragraph>
+            Clicking this link or icon will take you to the my cards page. <br />
+            Here, you can view all the cards you have created if you are a business user.
+          </Typography>
+          <Typography variant="h6" gutterBottom>
+            <SearchIcon /> Search
+          </Typography>
+          <Typography variant="body1" paragraph>
+            Clicking this icon will search for cards whose title includes the characters you enter in the search bar. <br />
+            Here, you can search for specific cards by entering keywords into the search bar.
+            The search functionality allows you to quickly find cards that match your criteria. It is only available on pages that display cards.
+          </Typography>
+          <Typography variant="h6" gutterBottom>
+            <AddIcon /> Add Card
+          </Typography>
+          <Typography variant="body1" paragraph>
+            Clicking this icon will take you to the add card page. <br />
+            To add a new card, navigate to any of the Cards pages and click on the
+            blue "+" button. This will open a form where you can enter
+            the details of the new card you are interested in creating. Once
+            you have filled out the form, click "Submit" to add the card to
+            your collection.
+          </Typography>
+          <Typography variant="h6" gutterBottom>
+            <EditIcon /> Edit Card
+          </Typography>
+          <Typography variant="body1" paragraph>
+            Clicking this icon will take you to the edit card page. <br />
+            To edit an existing card, go to any of the Cards pages in the navigation
+            bar and locate the card you want to edit. Remember, you may only
+            edit cards that only you have created. Click on the "Edit" button
+            associated with that card. This will open a form pre-filled with
+            the card's current details. Make the necessary changes and click
+            "Save" to update the card.
+          </Typography>
+          <Typography variant="h6" gutterBottom>
+            <DeleteIcon /> Delete Card
+          </Typography>
+          <Typography variant="body1" paragraph>
+            Clicking this icon will take you to the delete card page. <br />
+            If you need to delete a card, find the card on any of the Cards pages and
+            click the "Delete" button. You will be prompted to confirm the
+            deletion. Once confirmed, that card will be permanently removed
+            from your collection and the database.
+          </Typography>
+          <Typography variant="h6" gutterBottom>
+            <AppRegistrationIcon /> Register
+          </Typography>
+          <Typography variant="body1" paragraph>
+            Clicking this link will take you to the registration page. <br />
+            Once you have registered and become an active user on the site,
+            you will be able to access other features like adding cards,
+            editing cards, deleting cards, and marking cards as favorites. It
+            is important you sign up as a Business user, in order to access
+            these features.
+          </Typography>
+          <Typography variant="h6" gutterBottom>
+            <LoginIcon /> Login
+          </Typography>
+          <Typography variant="body1" paragraph>
+            Clicking this link will take you to the login page. <br />
+            To access certain features like managing favorites or your cards,
+            you need to be logged in. Use the login form to enter your
+            credentials.
+          </Typography>
+          <Typography variant="h6" gutterBottom>
+            <LogoutIcon /> Logout
+          </Typography>
+          <Typography variant="body1" paragraph>
+            Clicking this link will log you out of the application.
+          </Typography>
+          <Divider />
+          <Typography variant="h5" gutterBottom>
+            Theme
+          </Typography>
+          <Typography variant="body1" paragraph>
+            You can switch between light and dark themes by clicking on the
+            theme switch in the navigation bar.
+          </Typography>
+          <Divider />
+
         </Container>
         <Container sx={{ flex: 1 }}>
           <img
